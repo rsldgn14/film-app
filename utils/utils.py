d376b727bb7eae7ba9bc7476f461fca5fd2a9577
rsldgn14 <m.resuldogan14@gmail.com>
2022-12-13 00:24:16 +0300

    Film fotoğrafları eklendi

--- utils/utils.py
+++ utils/utils.py
@@ -12,6 +12,3 @@ def unique(list1):
             unique_list.append(x)
     return unique_list
 
-
-print(requests.get(
-    "https://m.media-amazon.com/images/M/MV5BMTAzOTBhNTAtMzY5MS00YjcwLWIzYWItNWI1NzQ4YjcxY2E2XkEyXkFqcGdeQXVyNTI4MjkwNjA@._V1_SX300.jpg").content)
