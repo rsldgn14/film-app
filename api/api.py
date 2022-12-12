2f3b0a054462663ac417bc90488a508c0fa29cd4
emrelagoz <98375201+emrelagoz@users.noreply.github.com>
2022-12-12 19:58:30 +0300

    Arayüz oluşturuldu.

---
+++ api/api.py
@@ -0,0 +1,14 @@
+import requests
+
+def getMovieByTitle(searchParam):
+    url = "https://imdb8.p.rapidapi.com/title/find"
+    querystring = {"q": searchParam}
+    headers = {
+        "X-RapidAPI-Key": "d19d3d8e07mshc2083cb18c1ab08p1b7266jsn2516f359fd7f",
+        "X-RapidAPI-Host": "imdb8.p.rapidapi.com"
+    }
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    title = response.json()
+    movies = title["results"]
+
+    return movies
