58f5e3753f709d05b8a12e1dccbfecf99beff127
egekrk <unsstabless@gmail.com>
2022-12-14 18:57:40 +0300

    Yorumlar siteden getirildi.

--- web-crawling/web_crawling.py
+++ web-crawling/web_crawling.py
@@ -1,21 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 from utils.utils import unique
-from api.api import getMovieByTitle
 
-res = getMovieByTitle("recep ivedik 7")
-imdbId = res["result"][0]["imdbID"]
-
-url = f"https://www.imdb.com/title/{imdbId}/reviews?ref=tt_ovrt"
-r = requests.get(url)
-soup = BeautifulSoup(r.content, "html.parser")
-rows = soup.find("div", _class="lister").find_all("div")
-"""all_data = []
+header = {
+    "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
+}
+url = f"https://www.imdb.com/title/tt19759204/reviews?ref=tt_ovrt"
+get = requests.get(url,headers=header)
+content = get.content
+soup = BeautifulSoup(content, "html.parser")
+rows = soup.find("div",class_="lister-list").find_all("div")
+all_data = []
 for row in rows:
-    temp = row.find("a", _class="title")
+    temp = row.find("a", class_="title")
     if temp:
-        price = temp.text.strip()
-        all_data.append(price)
-print(unique(all_data))"""
-print(rows)
-
+      price = temp.text.strip()
+      all_data.append(price)
+print(unique(all_data))
